d0ca78ace93201b19ecd41c728493bafe884c9bc
Brandon Ferguson <brandon.ferguson.ut@gmail.com>
2022-12-03 11:13:48 -0700

    About page

---
+++ client/src/components/shared/About.js
@@ -0,0 +1,13 @@
+const About = () = > (
+  <>
+    <h1>About Us</h1>
+    <h5>Something more about use</h5>
+
+    <h3>Meet the Team</h3>
+
+    <h4>Member 1</h4>
+    <img src='https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' width='100' alt='member'/>
+  </>
+)
+
+export default About;
