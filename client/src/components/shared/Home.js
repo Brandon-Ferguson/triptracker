7a7b089cb29e696ebe91ae59398ea52c01fad6fb
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2022-12-03 11:11:51 -0700

    donde est la bibloteca

---
+++ client/src/components/shared/Home.js
@@ -0,0 +1,14 @@
+import BoxFeat from './BoxFeat';
+import HomeHead from './HomeHead';
+import TextFeat from './TextFeat';
+
+const Home = () => (
+  <>
+    <HomeHead />
+    <TextFeat />
+    <BoxFeat />
+  </>
+)
+
+export default Home;
+# donde esta la bilioteca
